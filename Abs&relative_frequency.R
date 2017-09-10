f00f724479c768d01e918434fc09723c1c6fb95b
RGeetha24 <rgeetha2296@gmail.com>
2017-09-10 21:42:06 +0530

    Update Abs&relative_frequency.R

--- Abs&relative_frequency.R
+++ Abs&relative_frequency.R
@@ -4,3 +4,7 @@ gender<-c(1,2,1,2,1,1,1,2,1,1)
 gender
 table(gender) #Absolute frequency
 table(gender)/length(gender)#Relative frequency
+
+#Graphics(Visualize) is done by 'BAR PLOT'
+barplot(table(gender))#Absolute freq.
+barplot(table(gender)/length(gender))#Relative freq.
